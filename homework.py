f0e0d0ca2053c90ba2993df6187e537673df86f0
Егор Марецкий <emarpoint@gmail.com>
2021-10-11 13:35:59 +0300

    Изменил длину строки с 170 на менее 79 символов

--- homework.py
+++ homework.py
@@ -33,10 +33,11 @@ class CaloriesCalculator(Calculator):
     def get_calories_remained(self):
         calories_remained = self.get_today_remaind()
         if calories_remained > 0:
-            return f'Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более {calories_remained} кКал'
+            return 'Сегодня можно съесть что-нибудь ещё,'
+            f'но с общей калорийностью не более {calories_remained} кКал'
 
         else:
-            return f'Хватит есть!'
+            return 'Хватит есть!'
 
 
 class CashCalculator(Calculator):
